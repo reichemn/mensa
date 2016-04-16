a706c9712623b603d4463791b95434dc854d4824
Max <st143430@stud.uni-stuttgart.de>
2016-04-16 21:46:30 +0200

    New changes

--- Mensa/app/src/main/java/de/hackathon/mensa/MainActivity.java
+++ Mensa/app/src/main/java/de/hackathon/mensa/MainActivity.java
@@ -2,6 +2,9 @@ package de.hackathon.mensa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView txtTitle = (TextView) findViewById(R.id.txtTitle);
+        assert txtTitle != null;
+        txtTitle.setText("Hallo ");
+        txtTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hallo Welt", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
